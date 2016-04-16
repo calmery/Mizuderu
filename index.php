ffa9f82472dc9bce134c1e5c6f4207903a44a45a
Calmery <leaf0115.lab+io950@gmail.com>
2016-04-16 23:17:49 +0900

    Rewritten index.php

--- index.php
+++ index.php
@@ -1,73 +1,93 @@
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
+<!DOCTYPE html>
+<html lang="ja">
+    
+    <head>
+        <meta charset="utf-8">
+        <title>Watermap</title>
+        <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no">
+        <meta http-equiv="content-style-type" content="text/css">
+        <meta http-equiv="content-script-type" content="text/javascript">
+        <link rel="stylesheet" href="base.css">
+        <style>
+            #postBtn {
+                width: 100%;
+                height: 45px;
+                background: #fff;
+                text-align: center;
+                font-size: 20px;
+                font-weight: bold;
+                line-height: 34pt;
+                color: #000;
+            }
+        </style>
+    </head>
+    
+    <body>
+        
+        <a href="post.php">
+            <div id="postBtn">投稿する</div>
+        </a>
+        
+        <!-- View map -->
+        <div id="map"></div>
+        
+        <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+        <script>
+            
+            var map,
+                markers = ['no', 'ok', 'go']
+            
+            var m = document.getElementById( 'map' )
+            m.style.width  = window.innerWidth + 'px'
+            m.style.height = window.innerHeight - 45 + 'px' 
+            
+            map = new google.maps.Map( m, {
+                center: new google.maps.LatLng( 32.7858659,130.7633434 ),
+                zoom: 9,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            } )
+            
+            // Set Data
+            var position = <?php
+                
+                date_default_timezone_set( 'asia/tokyo' );
+                require_once( 'dbconnect.php' );
 
-<link rel="stylesheet" href="base.css">
-<style>
-    #post {
-        width: 100%;
-        height: 45px;
-        background: #fff;
-        text-align: center;
-        font-size: 20px;
-        font-weight: bold;
-        line-height: 34pt;
-        color: #000;
-    }
-</style>
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+                $connect = open_db();
 
-<a href="post.php">
-    <div id="post">投稿する</div>
-</a>
+                mysqli_query( $connect, 'SET NAMES utf8' );
+                mysqli_set_charset( $connect, 'utf8' );
 
-<div id="map"></div>
-<script type="text/javascript">
+                mysqli_select_db( $connect, '' );
 
-    var m = document.getElementById('map')
-    m.style.width = window.innerWidth + 'px'
-    m.style.height = window.innerHeight - 45 + 'px'
+                $res = mysqli_query( $connect, 'select * from info where time>16'. (date('m')-1). date('d') .'00' );
+                $json = '[';
+                while( $data = mysqli_fetch_array( $res ) ){
+                    $json = $json. '{locate:"'. $data['locate']. '",time:'. $data['time'] .',flg:'. $data['flg']. '},';
+                }
+                $json = $json. '{}]';
 
-    map = new google.maps.Map(m, { // #sampleに地図を埋め込む
-        center: new google.maps.LatLng(32.7858659,130.7633434), // 地図の中心を指定
-        zoom: 9, // 地図のズームを指定
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    })
+                echo $json;
 
-    var position = <?php
-
-        date_default_timezone_set('asia/tokyo');
-        require_once('dbconnect.php');
-
-        $connect = open_db();
-
-        mysqli_query($connect, "SET NAMES utf8");
-        mysqli_set_charset($connect, "utf8");
-
-        mysqli_select_db($connect, '');
-
-        $res = mysqli_query($connect, 'select * from info where time>16'. (date('m')-1). date('d') .'00');
-        $json = '[';
-        while( $usr = mysqli_fetch_array($res) ){
-            $json = $json. '{locate:"'. $usr['locate']. '",time:'. $usr['time'] .',flg:'. $usr['flg']. '},';
-        }
-        $json = $json. '{}]';
-
-        echo $json;
-
-        mysqli_close($connect);
-
-    ?>
-
-    console.log( position )
-
-    var arr
-    for( var i=0; i<position.length-1; i++ ){
-        arr = position[i]['locate'].split(',')
-        new google.maps.Marker({
-            position: new google.maps.LatLng(arr[0], arr[1]),
-            map: map,
-            icon: (position[i].flg == 1 ? "ok" : ( position[i].flg == 3?"go":"no") + ".png")
-        })
-    }
-
-</script>
+                mysqli_close( $connect );
+                
+            ?>
+            
+            // console.log( position ) // => [{Data},{Data}...,{}]
+                
+            var data
+            for( var i=0; i<position.length-1; i++ ){
+                data = position[i]['locate'].split(/,/)
+                console.log(position[i].flg)
+                new google.maps.Marker( {
+                    position: new google.maps.LatLng( data[0], data[1] ),
+                    map: map,
+                    icon: markers[position[i].flg]
+                } )
+            }
+            
+        </script>
+        
+    </body>
+    
+</html>
