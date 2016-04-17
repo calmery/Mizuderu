fae08fc77504ccbfab484a66b3353e6c67e44107
Shinobu Izumi <izumi@cis.sojo-u.ac.jp>
2016-04-17 09:18:13 +0900

    rds connection updated

--- dbconnect.php
+++ dbconnect.php
@@ -16,7 +16,7 @@ function open_db()
   } else {
     $dbhost = $_SERVER['RDS_HOSTNAME'];
     $dbport = $_SERVER['RDS_PORT'];
-    $dbname = $_SERVER['RDS_DB_NAME'];
+    $dbname = $_SERVER['RDS_MY_DB_NAME'];
 
     $dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname}";
     $username = $_SERVER['RDS_USERNAME'];
