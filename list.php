04778618e632b5f956a1a4222739692db299d91a
keisuke shingaki <kei4eva4@gmail.com>
2016-04-18 18:41:38 +0900

    fix

--- list.php
+++ list.php
@@ -22,48 +22,7 @@ while ($data = mysqli_fetch_array($res)) {
     $arr[] = $data;
 }
 
-//foreach($arr as &$a){
-//    $query = array("latlng" => $a["locate"]);
-//    $res = callApi("GET", "https://maps.googleapis.com/maps/api/geocode/json", $query);
-//
-//    $a["address"] = $res["results"][0]["formatted_address"];
-//}
 
 mysqli_close($connect);
 
 include 'views/list.php';
-
-
-function callApi($method, $url, $data = false)
-{
-    $curl = curl_init();
-
-    switch ($method)
-    {
-        case "POST":
-            curl_setopt($curl, CURLOPT_POST, 1);
-
-            if ($data)
-                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-            break;
-        case "PUT":
-            curl_setopt($curl, CURLOPT_PUT, 1);
-            break;
-        default:
-            if ($data)
-                $url = sprintf("%s?%s", $url, http_build_query($data));
-    }
-
-    // Optional Authentication:
-    curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-    curl_setopt($curl, CURLOPT_USERPWD, "username:password");
-
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-    $result = curl_exec($curl);
-
-    curl_close($curl);
-
-    return json_decode($result, true);
-}
