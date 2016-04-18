3b534d43127843e5990110db93841bf7cf5b98f7
KeisukeUtsumi <utsumi-k@klab.com>
2016-04-18 18:10:53 +0900

    DB設定

---
+++ config/env.php
@@ -0,0 +1,7 @@
+<?php
+list($user, $pass) = explode(":", trim(file_get_contents(AUTH_DIR . "/db")));
+define("DB_MASTER_DESTINATION", "water");
+//define("DB_DSN", "mysql:host=aa247qpcanfzbf.cjbydkyc8ulh.ap-northeast-1.rds.amazonaws.com;dbname=water;port=3306;charset=utf8");
+define("DB_DSN", "mysql:host=localhost;dbname=water;port=3306;charset=utf8;");
+define("DB_USERNAME", $user);
+define("DB_PASSWORD", $pass);
