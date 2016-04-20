28c589890f9f99952cf1254a81cad35c9fea1b21
keisuke shingaki <kei4eva4@gmail.com>
2016-04-20 11:17:18 +0900

    画像を別ページで開くように修正

--- webroot/index.php
+++ webroot/index.php
@@ -5,10 +5,16 @@ require_once("../bootstrap.php");
 $db = DB::conn();
 $now = time();
 $from_time = $now - (60 * 60 * 24 * 2);
-$rows = $db->rows("SELECT * FROM rousui WHERE time > ? ORDER BY time ASC", [$from_time]);
+$rows = $db->rows("SELECT * FROM info WHERE time > ? ORDER BY time ASC", [$from_time]);
+$r_rows = $db->rows("SELECT * FROM rousui WHERE time > ? ORDER BY time ASC", [$from_time]);
 
+if($rows[0]['time'] > $r_rows[0]['time']){
+    $base_time = $r_rows[0]['time'];
+}else {
+    $base_time = $rows[0]['time'];
+}
 
-$from_time = strtotime(getDateRound(date("Y-m-d H:i:s", $rows[0]['time']), 100, "floor"));
+$from_time = strtotime(getDateRound(date("Y-m-d H:i:s", $base_time), 100, "floor"));
 $now = strtotime(getDateRound(date("Y-m-d H:i:s", $now), 100, "ceil"));
 
 $template = Template::factory();
--- webroot/js/index.js
+++ webroot/js/index.js
@@ -49,7 +49,7 @@ function attachMessage(marker, post_time, flg, comment, rousui_image_url) {
         var rousui_img = "";
         // 漏水の画像があるなら表示
         if(flg == "rousui" && rousui_image_url !== "" && rousui_image_url !== null && rousui_image_url != "undefined"){
-            rousui_img = "<br>" + "<img src='" + rousui_image_url + "' width='200' alt='' >";
+            rousui_img = "<br>" + "<a href='" + rousui_image_url + "' target='_blank'><img src='" + rousui_image_url + "' width='200' alt='' ></a>";
         }
 
         new google.maps.Geocoder().geocode({
