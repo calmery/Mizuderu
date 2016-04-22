686fc3c389013f6ed69e0ae5de02a1dc115701b1
keisuke shingaki <kei4eva4@gmail.com>
2016-04-23 01:47:16 +0900

    漏水報告の画像必須を外す。

--- webroot/rousui_post.php
+++ webroot/rousui_post.php
@@ -1,10 +1,10 @@
 <?php
 require_once("../bootstrap.php");
-if( isset( $_POST['submit'] ) ){
+if (isset($_POST['submit'])) {
     $err = "";
 
-    $time    = time();
-    $locate  = (string)$_POST['locate'];
+    $time = time();
+    $locate = (string)$_POST['locate'];
     $comment = (string)$_POST['comment'];
     if (IsLocateString($locate) == false) {
         $err = "経度緯度情報が不正です";
@@ -12,19 +12,19 @@ if( isset( $_POST['submit'] ) ){
 
     if ($err === "") {
         $image_url = "";
-        if(isset($_FILES["image"])) {
+        if (isset($_FILES["image"]) && is_uploaded_file($_FILES["image"]["tmp_name"])) {
             // アップロード処理
             $file = $_FILES["image"];
 
-            if (!is_uploaded_file($file["tmp_name"])) {
-                die('ファイルがアップロードされていません');
-            }
+//            if (!is_uploaded_file($file["tmp_name"])) {
+//                die('ファイルがアップロードされていません');
+//            }
 
             if (!IsImage($file)) {
                 die('画像はJPEG(jpg,jpeg)、GIF(gif)、PNG(png)のいずれかとなっております。');
             }
             $savePath = safeImage($file["tmp_name"], TMP_DIR);
-            if ( $savePath === "" ){
+            if ($savePath === "") {
                 die("不正な画像がuploadされました");
             }
 
@@ -33,36 +33,36 @@ if( isset( $_POST['submit'] ) ){
             // 書きだした画像を削除
             @unlink($savePath);
 
-            if($result){
+            if ($result) {
                 $image_url = $result['ObjectURL'];
             }
         }
 
-        if ($image_url !== "") {
+        //if ($image_url !== "") {
 
-            $query = array(
-                "latlng" => h($locate),
-                "language" => "ja",
-                "sensor" => false
-            );
-            $res = callApi("GET", "https://maps.googleapis.com/maps/api/geocode/json", $query);
+        $query = array(
+            "latlng" => h($locate),
+            "language" => "ja",
+            "sensor" => false
+        );
+        $res = callApi("GET", "https://maps.googleapis.com/maps/api/geocode/json", $query);
 
-            $address= $res["results"][0]["formatted_address"];
+        $address = $res["results"][0]["formatted_address"];
 
-            $sql = "INSERT INTO rousui SET time = :time, locate = :locate, comment = :comment, image_url = :image_url, address = :address, flg = :flg";
-            $params = ["time"    => $time ,
-                "locate"  => $locate ,
-                "comment" => $comment ,
-                "image_url" => $image_url,
-                "address" => $address,
-                "flg" => 4,
-            ];
+        $sql = "INSERT INTO rousui SET time = :time, locate = :locate, comment = :comment, image_url = :image_url, address = :address, flg = :flg";
+        $params = ["time" => $time,
+            "locate" => $locate,
+            "comment" => $comment,
+            "image_url" => $image_url,
+            "address" => $address,
+            "flg" => 4,
+        ];
 
-            DB::conn()->query($sql , $params);
-            header('Location: index.php');
-        }
+        DB::conn()->query($sql, $params);
+        header('Location: index.php');
+        //}
     }
-    echo $err .PHP_EOL;
+    echo $err . PHP_EOL;
 }
 
 
