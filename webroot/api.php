9dd386353d8ab473d355f0fdba13bca5def2b678
Suguru Murakami <sugumura@gmail.com>
2016-04-23 12:23:42 +0900

    漏水はフィルター無しでチェックによる表示/非表示のみ制御する

--- webroot/api.php
+++ webroot/api.php
@@ -19,10 +19,10 @@ $rows =[];
 if (count($flgs) > 0 && ($flgs[0]) != "") {
 
     // rousui table
-    //if(in_array(4, $flgs)){
+    if(in_array(4, $flgs)){
         $sql = "SELECT * FROM rousui";
         $r_rows = DB::conn()->rows($sql, $params);
-    //}
+    }
 
     // info table
     $sql = "SELECT * FROM info WHERE time > ? AND time < ? ";
